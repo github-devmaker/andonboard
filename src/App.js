db0c9e5663c7d08d7bc89cb823a28ec2325ef877
github-devmaker <112181959+github-devmaker@users.noreply.github.com>
2023-03-21 23:15:56 +0700

    box one success

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { Grid } from '@mui/material';
-import { Container } from '@mui/system';
 function App() {
   return (
     <>
-      <div className='row h-[180px] bg-red-400'>
-        <div className='col-2 flex flex-col'>
-          <span>Andon Board</span>
-          <div className='flex-1 bg-white items-center flex justify-center text-9xl'>000</div>
+      <div className='grid grid-cols-4 bg-blue-900 text-white h-[150px]'>
+        <div className='flex flex-col px-3 pb-3'>
+          <div>Andon Board</div>
+          <div className='flex-1 grid place-items-center items-center   text-8xl text-yellow-400 font-semibold'>123</div>
         </div>
-        <div className='col-4'>
-          2AFI7
+        <div>
+        <div className='grid place-items-center h-100 font-semibold text-8xl'>
+            2AFI7
+          </div>
         </div>
-        <div className='col-2'>
-          RUN
+        <div className='bg-black text-8xl grid place-items-center font-semibold'>
+        RUN
         </div>
-        <div className='col-4'>
-          <div className=' flex flex-col h-100'>
+        <div>
+        <div className=' flex flex-col h-100'>
             <div className='flex-1 bg-green-500'>MM-DD-YYYY</div>
             <div className='flex-1 bg-pink-500'>HH:mm:ss</div>
           </div>
